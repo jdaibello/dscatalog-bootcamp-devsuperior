57b5c8e90e8691dcb5a42751a18407c72637b013
João Pedro <jpedrodaibello1999@outlook.com>
2020-11-12 12:03:24 -0300

    Basic beans validation

--- src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,8 +1,11 @@
 package com.devsuperior.dscatalog.dto;
 
+import javax.validation.constraints.NotBlank;
+
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 	
+	@NotBlank(message = "Campo obrigatório")
 	private String password;
 	
 	public UserInsertDTO() {
