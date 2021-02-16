d8729ace17298f581751060461d86e9b6fc1a629
João Pedro <jpedrodaibello1999@outlook.com>
2021-02-16 19:13:58 -0300

    Removing product and adding loading on product list

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,9 +1,11 @@
 import Pagination from 'core/components/Pagination';
 import { ProductsResponse } from 'core/types/Product';
-import { makeRequest } from 'core/utils/request';
-import React, { useEffect, useState } from 'react';
+import { makePrivateRequest, makeRequest } from 'core/utils/request';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import Card from '../Card';
+import CardLoader from '../Loaders/ProductCardLoader';
 
 const List = () => {
   const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
@@ -11,7 +13,7 @@ const List = () => {
   const [activePage, setActivePage] = useState(0);
   const history = useHistory();
 
-  useEffect(() => {
+  const getProducts = useCallback(() => {
     const params = {
       page: activePage,
       linesPerPage: 4,
@@ -26,8 +28,27 @@ const List = () => {
       })
   }, [activePage]);
 
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
+
   const handleCreate = () => {
     history.push('/admin/products/create');
+  }
+
+  const onRemove = (productId: number) => {
+    const confirm = window.confirm('Deseja realmente excluir este produto?');
+
+    if (confirm) {
+      makePrivateRequest({ url: `/products/${productId}`, method: 'DELETE' })
+      .then(() => {
+        toast.info('Produto excluído com sucesso!');
+        getProducts();
+      })
+      .catch(() => {
+        toast.error('Erro ao excluir produto!');
+      })
+    }
   }
 
   return (
@@ -36,9 +57,11 @@ const List = () => {
         ADICIONAR
       </button>
       <div className="admin-list-container">
-        {productsResponse?.content.map(product => (
-          <Card product={product} key={product.id} />
-        ))}
+        {isLoading ? <CardLoader /> : (
+          productsResponse?.content.map(product => (
+            <Card product={product} key={product.id} onRemove={onRemove} />
+          ))
+        )}
         {productsResponse && (
         <Pagination
           totalPages={productsResponse.totalPages}
