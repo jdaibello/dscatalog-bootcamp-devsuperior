acfc7484f0f4c6a2782eee3aea37f0140b0a6ebe
João Pedro <jpedrodaibello1999@outlook.com>
2021-02-28 17:55:25 -0300

    Costumizing API info - Swagger

--- backend/src/main/java/com/devsuperior/dscatalog/config/SwaggerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/SwaggerConfig.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,8 +19,21 @@ public class SwaggerConfig {
 	
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+				.paths(PathSelectors.any())
+				.build();
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("DSCatalog API Documentation")
+				.version("1.0")
+				.license("Apache License Version 2.0")
+				.contact(new Contact("João Pedro", "https://github.com/jdaibello/dscatalog-bootcamp-devsuperior", null))
+	            .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
 				.build();
 	}
 }
