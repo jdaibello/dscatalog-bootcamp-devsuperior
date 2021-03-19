cd0686440dc68fe869daf53e3a4b3853293bf7f8
João Pedro <jpedrodaibello1999@outlook.com>
2021-03-18 21:46:58 -0300

    Update Navbar folder name

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './core/assets/styles/custom.scss';
-import './app.scss';
 import Routes from './Routes';
+import './app.scss';
 
 const App = () => {
   return (
