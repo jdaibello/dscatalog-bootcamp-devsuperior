c706c0d619ec7c2aa69cb5a0fd7469117bc88fba
João Pedro <jpedrodaibello1999@outlook.com>
2021-05-01 17:00:24 -0300

    Finding products by name

--- mobile/src/pages/Catalog.tsx
+++ mobile/src/pages/Catalog.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { ScrollView } from 'react-native';
-import { ProductCard } from '../components';
+import React, { useState } from "react";
+import { ScrollView } from "react-native";
+import { ProductCard, SearchInput } from "../components";
 
-import productImage from '../assets/product.png';
-import { theme } from '../styles';
+import productImage from "../assets/product.png";
+import { theme } from "../styles";
 
 const products = [
   {
     id: 1,
     imgUrl: productImage,
-    name: "Computador Desktop - Intel Core i7",
+    name: "Smartphone",
     price: 2279.0,
   },
   {
@@ -36,13 +36,23 @@ const products = [
     name: "Computador Desktop - Intel Core i7",
     price: 2279.0,
   },
-]
+];
 
 const Catalog: React.FC = () => {
+  const [search, setSearch] = useState("");
+
+  const data =
+    search.length > 0
+      ? products.filter((product) =>
+          product.name.toLowerCase().includes(search.toLowerCase())
+        )
+      : products;
+
   return (
     <ScrollView contentContainerStyle={theme.scrollContainer}>
-      {products.map((product) => (
-        <ProductCard { ...product } />
+      <SearchInput placeholder="Nome do produto" search setSearch={setSearch} />
+      {data.map((product) => (
+        <ProductCard {...product} />
       ))}
     </ScrollView>
   );
