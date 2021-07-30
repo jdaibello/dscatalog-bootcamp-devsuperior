0514631929e817be211d03f236804cac4bea9568
João Pedro <jpedrodaibello1999@outlook.com>
2021-07-29 22:12:44 -0300

    Adding NavBar

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Home, Catalog, ProductDetails } from "../pages";
+import { NavBar } from "../components";
+import { colors, nav } from "../styles";
+import { Text } from "react-native";
 
 const Stack = createStackNavigator();
 
+const HeaderText: React.FC = () => <Text style={nav.leftText}>DS Catalog</Text>;
+
 const Routes: React.FC = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerTitle: "",
+        headerStyle: {
+          backgroundColor: colors.primary,
+        },
+        headerLeft: () => <HeaderText />,
+        headerRight: () => <NavBar />,
+      }}
+    >
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Catalog" component={Catalog} />
       <Stack.Screen name="ProductDetails" component={ProductDetails} />
--- mobile/src/styles/index.ts
+++ mobile/src/styles/index.ts
@@ -1,4 +1,6 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+
+const deviceWidth = Dimensions.get("window").width;
 
 const colors = {
   white: "#FFFFFF",
@@ -220,4 +222,34 @@ const theme = StyleSheet.create({
   },
 });
 
-export { colors, text, theme };
+const nav = StyleSheet.create({
+  leftText: {
+    color: colors.white,
+    fontWeight: "bold",
+    marginLeft: 20,
+  },
+  drawer: {
+    marginRight: 20,
+  },
+  options: {
+    width: deviceWidth,
+    height: 120,
+    backgroundColor: colors.primary,
+    marginTop: 125,
+    marginRight: -20,
+    padding: 20,
+    justifyContent: "space-between",
+  },
+  option: {
+    paddingVertical: 5,
+  },
+  textOption: {
+    color: colors.white,
+    textTransform: "uppercase",
+  },
+  textActive: {
+    fontWeight: "bold",
+  },
+});
+
+export { colors, text, theme, nav };
