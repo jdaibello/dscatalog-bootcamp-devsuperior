3a35eaf9ca6126472e46eb5b2a51cd39272514e5
João Pedro <jpedrodaibello1999@outlook.com>
2021-08-02 19:01:39 -0300

    Adding tab navigation

---
+++ mobile/src/pages/Admin/Products.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Products = () => {
+  return (
+    <View>
+      <Text>Tela de Produtos (tela temporária)</Text>
+    </View>
+  );
+};
+
+export default Products;
