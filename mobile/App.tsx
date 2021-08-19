b6ca92f2fd24d5184f91f73a15e263e745b81487
João Pedro <jpedrodaibello1999@outlook.com>
2021-08-19 20:43:01 -0300

    Continuous Delivery - Updating the app without going to the store

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,23 @@
-import 'react-native-gesture-handler'
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native'
-import Routes from './src/routes';
+import "react-native-gesture-handler";
+import React, { useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import * as Updates from "expo-updates";
+import Routes from "./src/routes";
 
 const App: React.FC = () => {
+  async function checkUpdates() {
+    const update = await Updates.checkForUpdateAsync();
+
+    if (update.isAvailable) {
+      await Updates.fetchUpdateAsync();
+      await Updates.reloadAsync();
+    }
+  }
+
+  useEffect(() => {
+    checkUpdates();
+  }, []);
+
   return (
     <NavigationContainer>
       <Routes />
